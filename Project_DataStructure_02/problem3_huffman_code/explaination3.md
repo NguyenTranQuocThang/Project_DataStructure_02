1723d864a74d40e127199bb45edea76477012834
Nguyentranquocthang <thangv3009@gmail.com>
2022-08-12 12:20:42 +0700

    solutuion for problem3

---
+++ Project_DataStructure_02/problem3_huffman_code/explaination3.md
@@ -0,0 +1,51 @@
+Before I'am really sorry about my solution, because it's hard to understand. I will explain it , hope u understand
+
+Explain a little special handle
+
+1. I use queue priority like recommend solution to order queue what i will insert data after each loop
+
+2. I use Stack and State to implement traverse tree with depth first search traversal
+   ( what you teach me in lecture )
+   ( I customize it - a little in State)
+
+3. U can see when i put element to queue i will put format: (value,count,key)
+   In here: count is element number auto increase 1 after each loop
+   I use that to prevent exception : Cast type error of PriorityQueue (I know u understand if i just put (value,key))
+
+   Furthermore I put value before to queue auto sort by value (frequence)
+
+Explain a function
+
+1.huffman_encoding
+Step1: I check case zero data and 1 element in text (text will encode) -> i will not explain it
+
+Step2: I loop all character and add it to dictionary with key and value to get frequence of each character
+
+Step3: I convert dictionary to PriorityQueue to help me sort
+
+Step4: I loop in queue
+
+Step5: I will get 2 element in queue(with frequence is lowest)
+-> Create two nodes what corresponding with node_left and right
+-> Create father node and set left right with 2 nodes (above)
+
+Step6: I will call function traverse_node() when loop in queue have size is one
+
+Step7: I get dictionary what have couple of key and value (key is characer and value is huffman code)
+
+Step8: I replace all character with huffman code
+
+2.huffman_decoding
+
+Step0: I check for case empty and case 1 character
+
+Step1: I iterate code
+
+Step2: I check if 0 -> left, 1 -> right and if get leaves -> add to new blank string
+
+Complexity of this code:
+Very complex :( - sorry, i'm very busy to make cleaner
+
+I think :
+encode: it is tree -> log(n) (actually greater a lot than log n )
+decode: O(n)
--- Project_DataStructure_02/problem3_huffman_code/problem3.py
+++ Project_DataStructure_02/problem3_huffman_code/problem3.py
@@ -1,8 +1,6 @@
 # https://cmps-people.ok.ubc.ca/ylucet/DS/Huffman.html
-from inspect import stack
-from platform import node
 import sys
-from queue import PriorityQueue, Queue
+from queue import PriorityQueue
 
 
 class Stack():
@@ -83,11 +81,14 @@ class TreeNode:
 def traverse_node(data):
     dic = {}
     stack = Stack()
-    node = data[1]
+    node = data[2]
     state = State(node)
     state.huffman_code = ""
     stack.push(state)
     while(node):
+
+        #
+
         if node.has_left_child() and not state.get_visited_left():
             huff_code_father = state.huffman_code
             state.set_visited_left()
@@ -103,42 +104,58 @@ def traverse_node(data):
             state.huffman_code = huff_code_father + '1'
             stack.push(state)
         else:
+            if(state.get_node().character is not None):
+                dic[state.get_node().character] = state.huffman_code
             stack.pop()
             if not stack.is_empty():
                 state = stack.top()
                 node = state.get_node()
             else:
                 node = None
+    return dic
 
 
 def huffman_encoding(data):
+
+    queue = PriorityQueue()
+
+    count = 0
+
     if len(data) == 0:
         return "", None
+
+    if len(data) == 1:
+        return '0', (0, 0, TreeNode(data, 1))
 
     characters = {}
 
     for i in data:
         characters[i] = characters.get(i, 0) + 1
-    queue = PriorityQueue()
 
     for key, value in characters.items():
-        queue.put((value, key))
+        queue.put((value, count, key))
+        count += 1
     # while not queue.empty():
     #     item = queue.get()
     #     print(item)
     while not queue.empty():
         if(queue.qsize() == 1):
-            traverse_node(queue.get())
-            return
+            node = queue.get()
+            dic = traverse_node(node)
+            for key, value in dic.items():
+                data = data.replace(key, value)
+            return data, node
 
         first = queue.get()
 
-        node_first = TreeNode(first[1], first[0])
+        node_first = first[2]
+        if(not isinstance(node_first, TreeNode)):
+            node_first = TreeNode(first[2], first[0])
 
         second = queue.get()
-
-        node_second = TreeNode(second[1], second[0])
-
+        node_second = second[2]
+        if(not isinstance(node_second, TreeNode)):
+            node_second = TreeNode(second[2], second[0])
         node_internal = TreeNode(
             None, node_first.frequence + node_second.frequence)
 
@@ -146,33 +163,68 @@ def huffman_encoding(data):
 
         node_internal.right = node_second
 
-        queue.put((node_internal.frequence, node_internal))
+        queue.put((node_internal.frequence, count, node_internal))
+        count += 1
 
-# def huffman_decoding(data, tree):
-#     pass
+
+def huffman_decoding(data, node):
+    if len(data) == 0 or node == None:
+        return ""
+    decode_data = ''
+    root = node[2]
+
+    # check case 1 character
+    if root.character is not None:
+        return root.character
+
+    for i in data:
+        if i == '0':
+            root = root.get_left_child()
+        else:
+            root = root.get_right_child()
+        if root.character is not None:
+            decode_data += root.character
+            root = node[2]
+    return decode_data
+
+    # for key, value in reversed(dic).items():
+    #     data = data.replace(value, key)
+    # return data
 
 
 # if _name_ == "_main_":
 #     codes = {}
-#     a_great_sentence = "The bird is the word"
-#     print("The size of the data is: {}\n".format(
-#         sys.getsizeof(a_great_sentence)))
-#     print("The content of the data is: {}\n".format(a_great_sentence))
-#     encoded_data, tree = huffman_encoding(a_great_sentence)
-#     print("The size of the encoded data is: {}\n".format(
-#         sys.getsizeof(int(encoded_data, base=2))))
-#     print("The content of the encoded data is: {}\n".format(encoded_data))
-#     decoded_data = huffman_decoding(encoded_data, tree)
-#     print("The size of the decoded data is: {}\n".format(
-#         sys.getsizeof(decoded_data)))
-#     print("The content of the encoded data is: {}\n".format(decoded_data))
+a_great_sentence = "The bird is the word"
+print("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+print("The content of the data is: {}\n".format(a_great_sentence))
+encoded_data, tree = huffman_encoding(a_great_sentence)
+print("The size of the encoded data is: {}\n".format(
+    sys.getsizeof(int(encoded_data, base=2))))
+print("The content of the encoded data is: {}\n".format(encoded_data))
+decoded_data = huffman_decoding(encoded_data, tree)
+print("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+print("The content of the encoded data is: {}\n".format(decoded_data))
 # Add your own test cases: include at least three test cases
 # and two of them must include edge cases, such as null, empty or very large values
-huffman_encoding('fpt')
-
 
 # Test Case 1
-
+a_great_sentence = "Fpt software Fpt software Fpt software Fpt software Fpt software Fpt software Fpt software Fpt software Fpt software Fpt software"
+print("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+print("The content of the data is: {}\n".format(a_great_sentence))
+encoded_data, tree = huffman_encoding(a_great_sentence)
+print("The size of the encoded data is: {}\n".format(
+    sys.getsizeof(int(encoded_data, base=2))))
+print("The content of the encoded data is: {}\n".format(encoded_data))
+decoded_data = huffman_decoding(encoded_data, tree)
+print("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+print("The content of the encoded data is: {}\n".format(decoded_data))
 # Test Case 2
-
+a_great_sentence = "a"
+encoded_data, tree = huffman_encoding(a_great_sentence)
+decoded_data = huffman_decoding(encoded_data, tree)
+print(a_great_sentence == decoded_data)
 # Test Case 3
+a_great_sentence = ""
+encoded_data, tree = huffman_encoding(a_great_sentence)
+decoded_data = huffman_decoding(encoded_data, tree)
+print(a_great_sentence == decoded_data)
